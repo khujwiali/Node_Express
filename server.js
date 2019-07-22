9f90e151ffed7ba64129bacca10616f8d56238fc
Mohammed Ali Khujwi <mohdalikhujwi@credenceanalytics.com>
2019-07-22 12:43:03 +0530

    reverted server.js

--- server.js
+++ server.js
@@ -9,19 +9,13 @@ var express = require('express');
 var app = express();
 
 if (!process.env.DISABLE_XORIGIN) {
-  console.log("1111");
   app.use(function(req, res, next) {
     var allowedOrigins = ['https://narrow-plane.gomix.me', 'https://www.freecodecamp.com'];
     var origin = req.headers.origin || '*';
-    console.log("req.headers.origin",req.headers.origin)
     if(!process.env.XORIG_RESTRICT || allowedOrigins.indexOf(origin) > -1){
-         console.log("origin",origin);
+         console.log(origin);
          res.setHeader('Access-Control-Allow-Origin', origin);
          res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    }
-    else{
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     }
     next();
   });
